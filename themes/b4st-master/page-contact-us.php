74da7991bdd05264d89fdbe5c83610f758e4308b
Maxwell Stephens <stephensm2011@gmail.com>
2018-07-31 00:10:00 +0000

    added bg video on contact page

--- themes/b4st-master/page-contact-us.php
+++ themes/b4st-master/page-contact-us.php
@@ -4,7 +4,12 @@
 
     
 <!-- START MAIN CONTENT -->
+    <div class="banner">
 <div class="main-content contact-bg">
+      <video autoplay loop muted class="banner__video" poster="/wp-content/themes/b4st-master/img/1fallback.jpg">
+        <source src="/wp-content/themes/b4st-master/img/newyorkvidwebm.webm" type="video/webm">
+        <source src="/wp-content/themes/b4st-master/img/newyorkvid4.mp4" type="video/mp4">
+      </video>
    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
     <div class="container">
         <div class="row">
@@ -24,5 +29,6 @@
     
    
 </div>
-<!-- END MAIN CONTENT -->
 <?php get_footer(); ?>
+    </div>
+<!-- END MAIN CONTENT -->
